6365b46d8d66ee538be0dae3dfecb8c47f124b2d
RohanBandara <rohancbandara@gmail.com>
2022-08-15 11:40:27 +0530

    Initiate project

---
+++ src/main/java/com/rcb/DemoSpringBootJpaH2WithSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.rcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootJpaH2WithSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootJpaH2WithSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcb/DemoSpringBootJpaH2WithSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootJpaH2WithSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
